6930f00eec302ddb7e3cd6fb9fffdbc789775d59
pranay <166876346+generalpranay@users.noreply.github.com>
2025-01-28 15:11:20 -0700

    ud=pdates

---
+++ app/week_2/page.js
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function page(){
+    return(
+<div>
+<h1>Pranay Bhalsod</h1>
+<h1> assignment 2 </h1>
+<p><Link href= "https://github.com/">https://github.com/ </Link></p>
+</div>)
+}
